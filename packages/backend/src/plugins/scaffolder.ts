a7187d5d049ff2b41f7928bd2e2df3150eee893e
Armel Soro <asoro@redhat.com>
2023-11-03 11:32:57 +0100

    Use latest version of odo custom actions plugin

--- packages/backend/src/plugins/scaffolder.ts
+++ packages/backend/src/plugins/scaffolder.ts
@@ -1,8 +1,10 @@
 import { CatalogClient } from '@backstage/catalog-client';
-import { createRouter } from '@backstage/plugin-scaffolder-backend';
+import {
+  createBuiltinActions,
+  createRouter,
+} from '@backstage/plugin-scaffolder-backend';
 import { Router } from 'express';
 import type { PluginEnvironment } from '../types';
-import { createBuiltinActions } from '@backstage/plugin-scaffolder-backend';
 import { ScmIntegrations } from '@backstage/integration';
 import { odoInitAction, odoAction } from '@rm3l/plugin-scaffolder-odo-actions';
 
@@ -21,7 +23,11 @@ export default async function createPlugin(
     reader: env.reader,
   });
 
-  const actions = [...builtInActions, odoInitAction(), odoAction()];
+  const actions = [
+    ...builtInActions,
+    odoInitAction(env.config.getOptionalConfig("odo")),
+    odoAction(env.config.getOptionalConfig("odo")),
+  ];
 
   return await createRouter({
     logger: env.logger,
